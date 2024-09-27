d845b3a0d08546b692b7e5bedd12f1a3c31db72b
Diego Gadelha <125842935+Diegogadelha92@users.noreply.github.com>
2024-09-26 23:51:11 -0300

    Delete Atv. Imposto directory

--- Atv. Imposto/src/imposto/ICMS.java
+++
@@ -1,9 +0,0 @@
-package br.com.subsistema.imposto;
-import br.com.subsistema.model.Produto;
-
-public class ICMS implements br.com.subsistema.imposto.Imposto {
-    @Override
-    public double calcular(Produto produto) {
-        return produto.getValor() * 0.18;
-    }
-}
--- Atv. Imposto/src/imposto/Imposto.java
+++
@@ -1,7 +0,0 @@
-package br.com.subsistema.imposto;
-
-import br.com.subsistema.model.Produto;
-
-public interface Imposto {
-    double calcular(Produto produto);
-}
